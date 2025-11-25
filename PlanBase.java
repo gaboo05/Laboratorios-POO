f266aa644581cebaa758f468c24837b4892bab23
gaboo05 <cargabrielhernandez@gmail.com>
2025-11-25 10:07:19 -0600

    Agregamos valores a los metodos de las subclases Plan Base y PlanPremium

--- PlanBase.java
+++ PlanBase.java
@@ -1,3 +1,9 @@
 public class PlanBase extends Plan {
-    
+    public PlanBase() {
+        this.duracionMaxima = 30;
+        this.maximoInvitados = 10;
+        this.maximoReunionesDiarias = 1;
+        this.maximoReunionesCalendario = 10;
+        this.maximoContactos = 50;
+    }
 }
--- PlanPremium.java
+++ PlanPremium.java
@@ -1,3 +1,9 @@
 public class PlanPremium extends Plan {
-        
+    public PlanPremium() {
+        this.duracionMaxima = 60;
+        this.maximoInvitados = 50;
+        this.maximoReunionesDiarias = 5;
+        this.maximoReunionesCalendario = 30;
+        this.maximoContactos = 100;
+    }
 }
