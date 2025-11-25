d4fbcc3f96f6ad168136b7af001db0ab89845997
gaboo05 <cargabrielhernandez@gmail.com>
2025-11-25 10:09:09 -0600

    Especificamos en un comentario que el valor para la duracion maxima de una reunion esta dada en minutos

--- PlanBase.java
+++ PlanBase.java
@@ -1,6 +1,6 @@
 public class PlanBase extends Plan {
     public PlanBase() {
-        this.duracionMaxima = 30;
+        this.duracionMaxima = 30; // la duracion esta dada en minutos
         this.maximoInvitados = 10;
         this.maximoReunionesDiarias = 1;
         this.maximoReunionesCalendario = 10;
--- PlanPremium.java
+++ PlanPremium.java
@@ -1,7 +1,7 @@
 public class PlanPremium extends Plan {
     public PlanPremium() {
-        this.duracionMaxima = 60;
-        this.maximoInvitados = 50;
+        this.duracionMaxima = 60; // la duracion esta dada en minutos
+        this.maximoInvitados = 50; 
         this.maximoReunionesDiarias = 5;
         this.maximoReunionesCalendario = 30;
         this.maximoContactos = 100;
