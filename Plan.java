541d4e09590f6515eb6b5fd6477e698eb81cd4b3
gaboo05 <cargabrielhernandez@gmail.com>
2025-11-25 09:59:35 -0600

    Agregamos los atributos y metodos de la superclase Plan

--- Plan.java
+++ Plan.java
@@ -1,3 +1,33 @@
 public abstract class Plan {
     
+    // Atributos de tipo protected para la superclase Plan
+    protected int duracionMaxima;
+    protected int maximoInvitados;
+    protected int maximoReunionesDiarias;
+    protected int maximoReunionesCalendario;
+    protected int maximoContactos;
+
+    // Constructor
+    protected Plan() { }
+
+    // Mêtodos de la clase Plan que va a heredar a las clases PlanBase y PlanPremium
+    public int getDuracionMaxima() {
+        return duracionMaxima;
+    }
+
+    public int getMaximoInvitados() {
+        return maximoInvitados;
+    }
+
+    public int getMaximoReunionesDiarias() {
+        return maximoReunionesDiarias;
+    }
+
+    public int getMaximoReunionesCalendario() {
+        return maximoReunionesCalendario;
+    }
+
+    public int getMaximoContactos() {
+        return maximoContactos;
+    }
 }
